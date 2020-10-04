97ab55137dcaebe2f986bba514c60e6603deec5c
Mohsen Kazemizade <e.m.kazemizade@gmail.com>
2020-10-04 09:58:47 +0330

    add:react-moment,components,image

--- src/TaskOne.js
+++ src/TaskOne.js
@@ -1,6 +1,12 @@
 import React from "react";
 import Album from "./components/Album";
+import Card from "./components/Card";
 
 export default function TaskOne() {
-  return <div>my app</div>;
+  return (
+    <div>
+      <Album />
+      <Card />
+    </div>
+  );
 }
